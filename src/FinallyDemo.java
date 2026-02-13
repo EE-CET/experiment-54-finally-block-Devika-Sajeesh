acdd89fc4ec85ccb4d1ffab9c13060788b46c71e
Devika-Sajeesh <devikasajeesh26@gmail.com>
2026-02-13 10:16:22 +0530

    initial commit

--- src/FinallyDemo.java
+++ src/FinallyDemo.java
@@ -1,7 +1,23 @@
 import java.util.Scanner;
 
 public class FinallyDemo {
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        try{
+            int result = a/b; // Enclose division in try block
+            System.out.println("Result: " + result);
+        }
+        catch(ArithmeticException e){ // Catch ArithmeticException
+            System.out.println("Error: / by zero");
+        }
+        finally{ // finally block
+            System.out.println("Finally block executed");
+        }
         // TODO: Read a and b
         
         // TODO: try block
